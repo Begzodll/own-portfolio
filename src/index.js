114e395033da2940ea9c91971721ea7d25c640c7
57073731sb <begzodsirojiddinov9@gmail.com>
2022-06-10 12:08:14 +0500

    index  fixed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './app'
+import App from './App'
 import '../node_modules/normalize.css'
 import { BrowserRouter } from 'react-router-dom'
 
